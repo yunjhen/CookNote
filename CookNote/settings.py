2142f0802ed52530b5d8a2c4b18efbe3691c6776
yunjhen <x123456864@gmail.com>
2023-01-31 23:00:01 +0800

    Fix The Secret Leak

--- CookNote/settings.py
+++ CookNote/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9i6@3#3ez366=+20-#l9zul0%x@j9k%m)#n^3du=4k_$$+o0gx'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
